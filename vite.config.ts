6fbe5fd1f6814990a30245da17d236fec83d63fe
Isaiah Martin <theisaiahmartin@gmail.com>
2025-10-21 18:30:28 -0700

    Vercel Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import commonjs from '@rollup/plugin-commonjs'; // Import the plugin
+// vite.config.ts
+import commonjs from '@rollup/plugin-commonjs';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
-    commonjs()
-  ]
+    commonjs({
+      // Ensure the commonjs plugin processes the expo modules
+      include: /node_modules/,
+    }),
+    tsconfigPaths(),
+  ],
+  optimizeDeps: {
+    // Specify the dependencies that should be pre-bundled by Vite
+    include: ['@expo/vector-icons'],
+  },
+  build: {
+    rollupOptions: {
+      // Ensure that problematic modules are externalized
+      // This is a last resort if other methods fail
+      external: ['react-native', 'react-native-vector-icons', '../../components/parallax-scroll-view', '@expo/vector-icons'],
+       },
+  },
 });
