6a50db411de0bcba0c41e4bec81b701de2df1f2b
Isaiah Martin <theisaiahmartin@gmail.com>
2025-11-04 12:42:23 -0800

    UI Dub Rev

--- app/(app)/(tabs)/Navigation.tsx
+++ app/(app)/(tabs)/Navigation.tsx
@@ -3,8 +3,8 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
 import LoadingScreen from '../../../loading';
 
-
-export const AppNavigator = () => {
+// Change the named export to a default export
+const AppNavigator = () => {
   const { isLoggedIn, loading } = useContext(AuthContext);
 
   if (loading) {
@@ -19,3 +19,5 @@ export const AppNavigator = () => {
     return <Redirect href="/(auth)/login" />;
   }
 };
+
+export default AppNavigator; // Export it as default
